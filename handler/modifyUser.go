3b2e0c1a2ec1fc6b6605121610264ba618054558
achange8 <95342381+achange8@users.noreply.github.com>
2022-05-23 18:30:29 +0900

    Update modifyUser.go
    not done

--- handler/modifyUser.go
+++ handler/modifyUser.go
@@ -2,10 +2,14 @@ package handler
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	db "github.com/achange8/Portfolio/DB"
 	"github.com/achange8/Portfolio/module"
+	"github.com/golang-jwt/jwt"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 )
 
@@ -21,15 +25,34 @@ func Modifyuser(c echo.Context) error {
 	saveEmail := user.Email
 	saveId := user.Id
 	savePW := user.Password
-
-	id := db.Raw("SELECT * FROM users WHERE id = ?", user.Id).Scan(&user)
-	if id.RowsAffected != 0 {
-		fmt.Println(user)
-		return c.JSON(http.StatusConflict, "ID already exists!")
+	envERR := godotenv.Load("db.env")
+	if envERR != nil {
+		log.Println("Could not load .env file")
+		os.Exit(1)
 	}
 
-	db.Model(&user).Where("email = ?", saveEmail).
-		Updates(map[string]interface{}{"id": saveId, "password": savePW})
+	cookie, err := c.Cookie("RefreCookie")
+	if err != nil {
+		return err
+	}
+	token := cookie.Value
+	refreshClaims := &jwt.StandardClaims{}
+	_, err = jwt.ParseWithClaims(token, refreshClaims,
+		func(t *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("key2")), nil
+		})
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, "jwt not allowed")
+	}
+	fmt.Println("parse done")
+	id := db.Raw("SELECT * FROM users WHERE id = ?", user.Id).Scan(&user)
+	if id.RowsAffected != 0 {
+		return c.JSON(http.StatusConflict, "ID already exists!")
+	} else {
+		pw, _ := module.HashPassword(savePW)
+		db.Model(&user).Where("email = ?", saveEmail).
+			Updates(map[string]interface{}{"id": saveId, "password": pw})
+	}
 
 	return c.JSON(http.StatusOK, user)
 }
