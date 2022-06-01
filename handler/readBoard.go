d6cdb47eef83bd8670217cc003b64c9f8418fd96
achange8 <95342381+achange8@users.noreply.github.com>
2022-06-01 18:21:46 +0900

    update readboard api (test done)
    test done

--- handler/readBoard.go
+++ handler/readBoard.go
@@ -11,16 +11,18 @@ import (
 	"gorm.io/gorm/clause"
 )
 
-//method : POST
+//method : GET
 //api:  /read/?id=num
+// lookup viewcookie value, view +1, scan board return board val
+//view history in cookie value
 func ReadBoard(c echo.Context) error {
 	id := c.QueryParam("id")
 	board := new(module.BOARD)
 	viewcookie, err := c.Cookie("viewCookie")
+	db := db.Connect()
 	if err != nil {
 		viewcookie = module.CreateViewCookie(id)
 		c.SetCookie(viewcookie)
-		db := db.Connect()
 		db.Model(&board).Clauses(clause.Returning{}).Where("NUM = ?", id).Update("hi_tcount", gorm.Expr("hi_tcount + ?", 1)).Scan(board)
 
 		return c.JSON(http.StatusOK, board)
@@ -31,12 +33,10 @@ func ReadBoard(c echo.Context) error {
 		newview := viewcookie.Value + "," + id
 		viewcookie = module.CreateViewCookie(newview)
 		c.SetCookie(viewcookie)
-		db := db.Connect()
 		db.Model(&board).Clauses(clause.Returning{}).Where("NUM = ?", id).Update("hi_tcount", gorm.Expr("hi_tcount + ?", 1)).Scan(board)
 
 		return c.JSON(http.StatusOK, board)
 	}
-	db := db.Connect()
 	db.Find(&board, "NUM =?", id).Scan(board)
 	return c.JSON(http.StatusOK, board)
 }
