ad94d1e550b4dd12700cfefd50b613fa37858554
achange8 <95342381+achange8@users.noreply.github.com>
2022-06-13 18:13:07 +0900

    refactoring code

--- module/duplicatecheck.go
+++ module/duplicatecheck.go
@@ -2,12 +2,11 @@ package module
 
 import db "github.com/achange8/Portfolio/DB"
 
+//post
 func Duplicate(id string) bool {
 	db := db.Connect()
 	user := new(User)
 	a := db.Find(&user, "id = ?", id)
-	if a.RowsAffected == 0 {
-		return true
-	}
-	return false
+
+	return a.RowsAffected == 0
 }
